21a4822041bcc9a4e1c51ce0d3ffe2f2dd5a91bc
Laional <cdokonstan@gmail.com>
2017-02-23 20:22:08 +0100

    - mas cambios

--- php/recibedatos.php
+++ php/recibedatos.php
@@ -3,13 +3,18 @@
 $test = 1;
 // para comprobar si se recibe un post desde un AJAX
 if ($_SERVER['REQUEST_METHOD'] === 'POST'){
+
 	//para almacenar los datos JSON recibidos en una variable
 	$request = file_get_contents('php://input');
+
+	//para convertir un JSON en un ARRAY PHP
+	$datos = json_decode($request,true);
+	
 	// codificar datos para enviar de vuelta con json
 	echo json_encode([
 		"resultado" => "ok",
 		"error" => 0,
-		"testeo" => $request
+		"testeo" => $datos
 		]);
 } else {
 	echo json_encode([
